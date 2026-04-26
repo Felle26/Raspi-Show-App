5178def686bdbff64111c5168a3d4bff1c930a10
Sebastian Felsberg <sebastianfelsberg@web.de>
2026-04-26 20:44:19 +0200

    Füge Unterstützung für das Abrufen von Zeichnungen und PDF-Dateien hinzu; implementiere Fehlerbehandlung und sichere Dateipfade

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-module.exports = {
   output: "standalone",
+  allowedDevOrigins: ["http://192.168.178.170:3000"],
 };
 
 export default nextConfig;
